0f955d63231639296c3a543248b729119f3340fc
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-12 22:59:13 -0500

    Update

--- punto_4.py
+++ punto_4.py
@@ -1,5 +1,11 @@
-"""Show the number of people in a family for each generation
-(up to a given generation).
+"""La siguiente gráfica muestra el comportamiento de los descendientes y ascendientes de una persona,
+si asumimos que esta persona es la generación 0, la generación 1 serán dos personas (sus padres)
+la generación 2 serán 4 personas (sus abuelos) y así sucesivamente.
+
+Debe crear un programa que dada una generación (mayor o igual a cero):
+- Le indique al usuario el número total de personas de la familia
+(de todas las generaciones hasta la generación dada).
+- Muestre el número de personas de cada generación mientras hace el cálculo.
 """
 
 generation = int(input('Generación: '))
@@ -12,5 +18,3 @@ else:
         people = 2**(i)
         print(f'En la generación {i} hay {people} personas.')
     print(f'El número total de personas, hasta la generación {i+1}, es: {2**(i+1)}.')
-
-
